a4b99be52a067bba01c1b97fc9e46c43d96a6030
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-03 23:05:26 +0300

    chore: Make git to build instead of using `bin` release

--- git-install.sh
+++ git-install.sh
@@ -1,7 +1,11 @@
 #/bin/sh
 
-# Find the OS type and store it in a variable
+git_version=$(curl "https://api.github.com/repos/git/git/tags" -s | jq -r '.[0].name')
+github_repo="https://github.com/git/git"
 
+# Find the OS type and store it in a variable
+# Some of the installations are nearly the same in different OS
+# If you spot any, please combine to reduce redundancy
 function findOsType () {
 	if [ $OSTYPE == "linux-gnu" ]; then
 		osType="Linux"
@@ -16,52 +20,87 @@ function findOsType () {
 	elif [ $OSTYPE == "freebsd"* ]; then
 		osType="FreeBSD"
 	else
-		osType="Unknown"
+		echo "Your OS is not supported. Please install git manually."
+		echo "Head over to $github_repo for installation"
+		exit 1
 	fi
 }
 
 findOsType
-echo "af: findos"
+
 # Find the distribution of the Linux system
 
-function linux_FindDistro(){
-	linux_distro=$(cat /etc/*-release 2> /dev/null | grep -oP '^ID_LIKE=\K.*' | tr '[[:upper:]]' '[[:lower:]]' )
+function FindDiff(){
+	if [ $osType == "Linux" ]; then
+		linux_distro=$(cat /etc/*-release 2> /dev/null | grep -oP '^ID_LIKE=\K.*' | tr '[[:upper:]]' '[[:lower:]]' )
+	fi
 }
 
 # Install git on the Linux system
 
+wget https://github.com/git/git/archive/refs/tags/v2.41.0.tar.gz
+
+function make_git() {
+		# Check if wget is installed, if not install it
+		if ! command -v wget 2>&1; then
+			$1 install wget
+		fi
+
+		# Download the latest "git tag"
+		wget https://github.com/git/git/archive/refs/tags/$git_version.tar.gz
+
+		# Check if tar is available
+		if ! command -v tar 2>&1; then
+			$1 install tar
+		fi
+
+		# Unarchive the file for use in the installation
+		tar -xf $git_version.tar.gz
+
+		# Check if make is available
+		if ! command -v make 2>&1; then
+			$1 install make
+		fi
+
+		# Switch to the git folder
+		cd $git_version
+
+		# This is a "faster" method of building git, if you prefer you can do a progile build
+		# Building profile takes a lot of time so I won't attempt to do it here
+
+		make prefix=/usr all doc info
+		sudo make prefix=/usr install install-doc install-html install-info
+
+}
+
 if [ $osType == "Linux" ]; then
-	linux_FindDistro
-	if [ $linux_distro == "debian" ]; then
-		apt-get install git
-	elif [ $linux_distro == "ubuntu" ]; then
-		add-apt-repository ppa:git-core/ppa
-		apt update; apt install git
+	FindDiff
+	if [ $linux_distro == "debian" || $linux_distro == "ubuntu" ]; then
+		make_git apt
+
 	elif [ $linux_distro == "fedora" ]; then
 		if command -v dnf > /dev/null 2>&1; then
-			dnf install git
+			make_git dnf
 		else
-			yum install git
+			make_git yum
 		fi
 	elif [ $linux_distro == "arch" || $linux_distro == "arch linux" ]; then
-		pacman -S git
+		make_git pacman
 	elif [ $linux_distro == "gentoo" ]; then
-		emerge --ask --verbose dev-vcs/git
+		make_git emerge
 	elif [ $linux_distro == "opensuse" ]; then
-		zypper install git
+		make_git zypper
 	elif [ $linux_distro == "alpine" ]; then
-		apk add git
+		make_git apk
 	elif [ $linux_distro == "openbsd" ]; then
-		pkg_add git
+		make_git pkg_add
 	elif [ $linux_distro == "slitaz" ]; then
-		tazpkg get-install git
+		make_git tazpkg
+	else
+		make_git "" 2>&1
+		if [ $? -ne 0 ]; then
+			echo "Could not install git, head over to $github_repo for installation"
+			exit 1
 	fi
 
-fi
-
-
-if [ $osType == "Unkown" ]; then
-	echo "Your OS is not supported. Please install git manually."
-	echo "Head over to https://github.com/git/git or https://git-scm.com/downloads"
-	exit 1
 fi
