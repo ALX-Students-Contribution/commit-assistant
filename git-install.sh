1b44bc6ea70627f4b05f041b540c2e689fe84c47
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-03 18:52:18 +0300

    fix: Add a missing if

--- git-install.sh
+++ git-install.sh
@@ -21,46 +21,47 @@ function findOsType () {
 }
 
 findOsType
-
+echo "af: findos"
 # Find the distribution of the Linux system
 
 function linux_FindDistro(){
 	linux_distro=$(cat /etc/*-release 2> /dev/null | grep -oP '^ID_LIKE=\K.*' | tr '[[:upper:]]' '[[:lower:]]' )
-
 }
 
 # Install git on the Linux system
 
 if [ $osType == "Linux" ]; then
 	linux_FindDistro
-	if [ $linux_distro == 'debian' ]; then
+	if [ $linux_distro == "debian" ]; then
 		apt-get install git
-	if [ $linux_distro == 'ubuntu' ]; then
+	elif [ $linux_distro == "ubuntu" ]; then
 		add-apt-repository ppa:git-core/ppa
 		apt update; apt install git
-	if [ $linux_distro == 'fedora' ]; then
+	elif [ $linux_distro == "fedora" ]; then
 		if command -v dnf > /dev/null 2>&1; then
 			dnf install git
 		else
 			yum install git
 		fi
-	if [ $linux_distro == 'arch' || $linux_distro == 'arch linux' ]; then
+	elif [ $linux_distro == "arch" || $linux_distro == "arch linux" ]; then
 		pacman -S git
-	if [ $linux_distro == 'gentoo' ]; then
+	elif [ $linux_distro == "gentoo" ]; then
 		emerge --ask --verbose dev-vcs/git
-	if [ $linux_distro == 'opensuse' ]; then
+	elif [ $linux_distro == "opensuse" ]; then
 		zypper install git
-	if [ $linux_distro == 'alpine' ]; then
+	elif [ $linux_distro == "alpine" ]; then
 		apk add git
-	if [ $linux_distro == 'openbsd' ]; then
+	elif [ $linux_distro == "openbsd" ]; then
 		pkg_add git
-	if [ $linux_distro == 'slitaz' ]; then
+	elif [ $linux_distro == "slitaz" ]; then
 		tazpkg get-install git
+	fi
 
 fi
 
 
-if [ $osType == 'Unkown' ]; then
+if [ $osType == "Unkown" ]; then
 	echo "Your OS is not supported. Please install git manually."
+	echo "Head over to https://github.com/git/git or https://git-scm.com/downloads"
 	exit 1
 fi
